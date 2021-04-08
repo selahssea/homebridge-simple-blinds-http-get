ad3fc7992744c7773bb832827abe5e52cc9d9d12
selahssea <>
2021-04-07 22:20:25 -0700

    Slat plugin created

---
+++ index.js
@@ -0,0 +1,87 @@
+var request = require("request");
+
+module.exports = (api) => {
+  api.registerAccessory('SimpleBlindsHttpGetPlugin', SimpleBlindsHttpGetPlugin);
+};
+
+class SimpleBlindsHttpGetPlugin {
+
+  constructor(log, config, api) {
+      this.log = log;
+      this.config = config;
+      this.api = api;
+
+      this.openURL = config.open_url;
+      this.closeURL = config.close_url;
+      this.statusURL = config.status_url;
+
+      this.Service = this.api.hap.Service;
+      this.Characteristic = this.api.hap.Characteristic;
+
+      // extract name from config
+      this.name = config.name;
+
+      // create a new Slats service
+      this.service = new this.Service(this.Service.Slats);
+
+      // create handlers for required characteristics
+      this.service.getCharacteristic(this.Characteristic.CurrentSlatState)
+        .onGet(this.handleCurrentSlatStateGet.bind(this));
+
+      this.service.getCharacteristic(this.Characteristic.SlatType)
+        .onGet(this.handleSlatTypeGet.bind(this));
+
+      this.service.getCharacteristic(this.Characteristic.SwingMode)
+        .onGet(this.handleSwingModeGet.bind(this))
+        .onSet(this.handleSwingModeSet.bind(this));
+
+  }
+
+  handleSwingModeSet(value) {
+    value ? this.run(this.openURL, resp => {
+      this.log(`Opening response: ${resp}`);
+    }) : this.run(this.closeURL, () => {
+      this.log(`Closing response: ${resp}`);
+    })
+  }
+
+  /**
+   * Handle requests to get the current value of the "Current Slat State" characteristic
+   */
+  handleCurrentSlatStateGet() {
+    this.log.debug('Triggered GET CurrentSlatState');
+
+    // set this to a valid value for CurrentSlatState
+    const currentValue = this.Characteristic.CurrentSlatState.SWINGING;
+
+    return currentValue;
+  }
+
+
+  /**
+   * Handle requests to get the current value of the "Slat Type" characteristic
+   */
+  handleSlatTypeGet() {
+    this.log.debug('Triggered GET SlatType');
+
+    // set this to a valid value for SlatType
+    const currentValue = this.Characteristic.SlatType.VERTICAL;
+
+    return currentValue;
+  }
+
+  run(commandUrl, callback, errorCallback) {
+    request({
+        method: "GET",
+        url: commandUrl,
+    }, function (err, response, body) {
+        if (!err && response && response.statusCode == 200) {
+            callback(body);
+        } else {
+            errorCallback(`error: ${err.message}, body: ${body}`);
+        }
+    });
+  }
+
+
+}
---
+++ index2.js
@@ -0,0 +1,111 @@
+const request = require('request');
+
+module.exports = (api) => {
+    api.registerAccessory('SimpleBlindsHttpGetPlugin', SimpleBlindsHttpGetPlugin);
+  };
+  
+  class SimpleBlindsHttpGetPlugin {
+  
+    constructor(log, config, api) {
+        this.log = log;
+        this.config = config;
+        this.openURL = config.open_url;
+        this.openURL = config.open_url;
+        this.statusURL = config.status_url;
+        this.api = api;
+
+        this.targetPosition = 0; // 0 – 100
+        this.currentPosition = 100;
+  
+        this.Service = this.api.hap.Service;
+        this.Characteristic = this.api.hap.Characteristic;
+  
+        // extract name from config
+        this.name = config.name;
+  
+        // create a new Window Covering service
+        this.service = new this.Service(this.Service.WindowCovering);
+  
+        // create handlers for required characteristics
+        this.service.getCharacteristic(this.Characteristic.CurrentPosition)
+          .onGet(this.handleCurrentPositionGet.bind(this));
+  
+        this.service.getCharacteristic(this.Characteristic.PositionState)
+          .onGet(this.handlePositionStateGet.bind(this));
+  
+        this.service.getCharacteristic(this.Characteristic.TargetPosition)
+          .onGet(this.handleTargetPositionGet.bind(this))
+          .onSet(this.handleTargetPositionSet.bind(this));
+  
+    }
+  
+    /**
+     * Handle requests to get the current value of the "Current Position" characteristic
+     */
+    handleCurrentPositionGet() {
+      this.log.debug('Triggered GET CurrentPosition');
+
+      let currentValue = 1;
+
+      this.run(this.statusURL, status => {
+        if (status == 0) {
+            this.log(`Current status CLOSED`);
+            currentValue = 0
+        } else if (status == 1) {
+            this.log(`Current status OPENED`);
+            currentValue = 100
+        }
+      })
+
+      this.currentPosition = currentValue;
+  
+      return currentValue;
+    }
+  
+  
+    /**
+     * Handle requests to get the current value of the "Position State" characteristic
+     */
+    handlePositionStateGet() {
+      this.log.debug('Triggered GET PositionState');
+  
+      // set this to a valid value for PositionState
+      const currentValue = this.Characteristic.PositionState.DECREASING;
+  
+      return currentValue;
+    }
+  
+  
+    /**
+     * Handle requests to get the current value of the "Target Position" characteristic
+     */
+    handleTargetPositionGet() {
+      this.log.debug('Triggered GET TargetPosition');
+  
+      // set this to a valid value for TargetPosition
+      const currentValue = 1;
+  
+      return currentValue;
+    }
+  
+    /**
+     * Handle requests to set the "Target Position" characteristic
+     */
+    handleTargetPositionSet(value) {
+      this.log.debug('Triggered SET TargetPosition:', value);
+    }
+
+    run(commandUrl, callback, errorCallback) {
+        request({
+            method: "GET",
+            url: commandUrl,
+        }, function (err, response, body) {
+            if (!err && response && response.statusCode == 200) {
+                callback(body);
+            } else {
+                errorCallback(`error: ${err.message}, body: ${body}`);
+            }
+        });
+    }
+  
+  }
